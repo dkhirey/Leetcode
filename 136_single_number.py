398921917e2a1ee5661d3f1eb039f8d1a5e5251c
deepakkhirey <34321733+dkhirey@users.noreply.github.com>
2025-01-01 17:21:16 +0530

    BIT MANIPULATION APPROACH

--- 136_single_number.py
+++ 136_single_number.py
@@ -1,20 +1,28 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # maintain a dictinary to keep count
-        sdict = {}
-        for num in nums:
-            # if its first time set the counter to 1 else increase counter
-            if num not in sdict.keys():
-                sdict[num] = 1
-            else :
-                sdict[num] += 1
-        # go over the dictionary to find which one appears only once
-        for k in sdict.keys():
-            if sdict[k] == 1:
-                return k
-        return None
+        # sdict = {}
+        # for num in nums:
+        #     # if its first time set the counter to 1 else increase counter
+        #     if num not in sdict.keys():
+        #         sdict[num] = 1
+        #     else :
+        #         sdict[num] += 1
+        # # go over the dictionary to find which one appears only once
+        # for k in sdict.keys():
+        #     if sdict[k] == 1:
+        #         return k
+        # return None
 
 ### This is a very straight forward solution
-# Time complexity is O(n) - to go over nums + to go over dictionary
+# Time complexity is O(n) - to go over nums + to go over dictionary 20ms
 # space complexity can be improved as dictionary has to be maintained
-        
+
+#---------------------------------------------------------#
+### BIT MANIPULATION APPROACH
+
+        bit = 0
+        for num in nums:
+            bit = bit ^ num
+        return bit
+# runtime complexity 4ms  
