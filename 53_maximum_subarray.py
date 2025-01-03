da5a3d5c15e3d8a4a867f33dea023abb53135934
deepakkhirey <34321733+dkhirey@users.noreply.github.com>
2025-01-03 14:40:12 +0530

    Implement solution for Maximum Subarray problem

---
+++ 53_maximum_subarray.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ## intitiating variables for local and global max
+        ## starting alues with first element as 
+        # it will take care of edge case where n = 1
+        seqsum = nums[0]
+        mxsum = nums[0]
+        # print(nums)
+        ## loop over rest of the elements
+        ## here lopp starts from 1 as we have already taken care of 0th element
+        for i in range(1,len(nums)):
+            # print(nums[i],"...",seqsum,"...",mxsum)
+            ## if current element is greater than sum of sequence sum + current element
+            ## this takes care of negative numbers and suquence sum
+            if nums[i] >= seqsum + nums[i]:
+                seqsum = nums[i]
+            else:
+                ## if current element is not greater than sum of sequence sum + current element
+                # usual case 
+                seqsum = seqsum + nums[i]
+            ## set global max to maximum of sequence sum and global max
+            mxsum = max(seqsum,mxsum)
+        # print(mxsum)
+        return mxsum
+    
+## simple solution with O(n) runtime complexity 66ms
